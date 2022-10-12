292efbf9249f666945b5c32b1da4a913dc30ceb2
Mateusz Bryła <brylamat@gmail.com>
2022-10-12 12:06:47 +0200

    feat: progress bar done

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -34,21 +34,35 @@ const Wrapper = styled.div`
   box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
 `;
 
+const ProgressMask = styled.div`
+  width: 100%;
+  height: 100%;
+
+  border-radius: 4px;
+  overflow: hidden;
+`;
+
 const Progress = styled.div`
   width: ${(props) => props.value}%;
   height: 100%;
 
-  border-radius: 4px 0px 0px 4px;
   background-color: ${COLORS.primary};
 `;
 
 const ProgressBar = ({ value, size }) => {
   const sizeVariables = SIZES_STYLES[size];
+  const cappedValue = value < 0 ? 0 : value > 100 ? 100 : value;
 
   return (
-    <Wrapper style={sizeVariables} role="progressbar" aria-valuenow={value}>
-      <VisuallyHidden>Progress: {value}%</VisuallyHidden>
-      <Progress value={value} />
+    <Wrapper
+      style={sizeVariables}
+      role="progressbar"
+      aria-valuenow={cappedValue}
+    >
+      <VisuallyHidden>Progress: {cappedValue}%</VisuallyHidden>
+      <ProgressMask>
+        <Progress value={value} />
+      </ProgressMask>
     </Wrapper>
   );
 };
