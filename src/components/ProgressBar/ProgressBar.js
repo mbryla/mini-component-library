53ea020f465ffc3bbd16a0f35963c1496fff4974
Mateusz Bryła <brylamat@gmail.com>
2022-10-12 11:56:50 +0200

    feat: progress bar - first try

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -1,12 +1,56 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-import { COLORS } from '../../constants';
-import VisuallyHidden from '../VisuallyHidden';
+import { COLORS } from "../../constants";
+import VisuallyHidden from "../VisuallyHidden";
+
+const SIZES_STYLES = {
+  large: {
+    "--height": "24px",
+    "--border-radius": "8px",
+    "--padding": "4px",
+  },
+  medium: {
+    "--height": "12px",
+    "--border-radius": "4px",
+    "--padding": "0",
+  },
+  small: {
+    "--height": "8px",
+    "--border-radius": "4px",
+    "--padding": "0",
+  },
+};
+
+const Wrapper = styled.div`
+  width: 370px;
+  height: var(--height);
+
+  padding: var(--padding);
+  border-radius: var(--border-radius);
+
+  background-color: ${COLORS.transparentGray15};
+  box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
+`;
+
+const Progress = styled.div`
+  width: ${(props) => props.value}%;
+  height: 100%;
+
+  border-radius: 4px 0px 0px 4px;
+  background-color: ${COLORS.primary};
+`;
 
 const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+  const sizeVariables = SIZES_STYLES[size];
+
+  return (
+    <Wrapper style={sizeVariables} role="progressbar" aria-valuenow={value}>
+      <VisuallyHidden>Progress: {value}%</VisuallyHidden>
+      <Progress value={value} />
+    </Wrapper>
+  );
 };
 
 export default ProgressBar;
