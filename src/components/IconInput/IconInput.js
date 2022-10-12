97d587f509ab962fd7115dea555bbec5323ed4f3
Mateusz Bryła <brylamat@gmail.com>
2022-10-12 18:23:41 +0200

    refactor: improved styling

--- src/components/IconInput/IconInput.js
+++ src/components/IconInput/IconInput.js
@@ -8,36 +8,29 @@ import VisuallyHidden from "../VisuallyHidden";
 
 const WRAPPER_SIZE_STYLES = {
   small: {
-    "--border-width": "1px",
     "--font-size": `${14 / 16}rem`,
   },
   large: {
-    "--border-width": "2px",
     "--font-size": `${21 / 16}rem`,
   },
 };
 
 const INPUT_SIZE_STYLES = {
   small: {
+    "--border-width": "1px",
     "--padding": "4px 0 4px 24px",
   },
   large: {
-    "--padding": "8px 0 8px 32px",
+    "--border-width": "2px",
+    "--padding": "8px 0 8px 36px",
   },
 };
 
-const Wrapper = styled.div`
+const Wrapper = styled.label`
+  display: block;
   position: relative;
-  width: ${(props) => props.width}px;
 
-  border-bottom: var(--border-width) solid ${COLORS.black};
   font-size: var(--font-size);
-
-  &:focus-within {
-    outline: 2px solid -webkit-focus-ring-color;
-    outline-offset: 2px;
-  }
-
   color: ${COLORS.gray700};
   &:hover {
     color: ${COLORS.black};
@@ -45,31 +38,30 @@ const Wrapper = styled.div`
 `;
 
 const StyledIcon = styled(Icon)`
-  pointer-events: none;
+  width: ${(props) => props.size};
 
+  margin: auto;
   position: absolute;
   top: 0;
   bottom: 0;
   left: 0;
-  margin: auto;
-  width: ${(props) => props.size};
 `;
 
 const StyledInput = styled.input`
+  width: ${(props) => props.width}px;
   padding: var(--padding);
-  font-size: inherit;
+
   border: none;
+  border-bottom: var(--border-width) solid ${COLORS.black};
+  outline-offset: 2px;
 
   color: inherit;
   font-weight: 700;
+  font-size: inherit;
 
   ::placeholder {
     color: ${COLORS.gray500};
     font-weight: 400;
-  }
-
-  :focus {
-    outline: none;
   }
 `;
 
@@ -78,7 +70,7 @@ const IconInput = ({ label, icon, width = 250, size, placeholder }) => {
   const inputSizeVariables = INPUT_SIZE_STYLES[size];
 
   return (
-    <Wrapper width={width} style={wrapperSizeVariables}>
+    <Wrapper style={wrapperSizeVariables}>
       <VisuallyHidden>{label}</VisuallyHidden>
       <StyledIcon
         id={icon}
@@ -89,6 +81,7 @@ const IconInput = ({ label, icon, width = 250, size, placeholder }) => {
         type="text"
         placeholder={placeholder}
         style={inputSizeVariables}
+        width={width}
       />
     </Wrapper>
   );
